d85c16e0bc4a36cc79cc56ec5542ccc85afdbd21
Lucas <lucaslops12@gmail.com>
2025-11-18 15:32:45 -0300

    add react toast

---
+++ src/utils/toast.ts
@@ -0,0 +1,30 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // needs the import because the tailwind css
+
+
+
+export const showToast = (msg: string, status: "error" | null = null) => {
+  if (!status) {
+    toast.success(msg, {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  } else if (status === "error") {
+    toast.error(msg, {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
+};
